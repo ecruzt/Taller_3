8527672240f00d0596402859f488fa015bb8da36
Emanuel Cruz Tabares <169178781+ecruzt@users.noreply.github.com>
2024-05-07 18:35:01 -0500

    agregué los archivos de trabajo

---
+++ funtions.py
@@ -0,0 +1,78 @@
+data_medicos = r'C:\Users\VICTUS\Desktop\UdeA\Tercer semestre\Informática\Tercer_parcial\Taller_monitor\Datos\medicos.csv'
+data_pacientes = r'C:\Users\VICTUS\Desktop\UdeA\Tercer semestre\Informática\Tercer_parcial\Taller_monitor\Datos\pacientes.json'
+data_resultados = r'C:\Users\VICTUS\Desktop\UdeA\Tercer semestre\Informática\Tercer_parcial\Taller_monitor\Datos\resultados.txt'
+
+
+def read_csv(archivo):
+    import csv
+    try:
+        with open(archivo, 'r') as medicos:
+            medi = csv.DictReader(medicos)
+            contenido_medicos = {}
+            for i, row in enumerate(medi):
+                contenido_medicos[i]=(row['cedula'], row['nombre'], row['codigo'])
+        return contenido_medicos
+    except FileNotFoundError:
+        print(f"El archivo {archivo} no se encontró.")
+        return None
+    except KeyError as e:
+        print(f"La clave {e} no se encontró en el archivo.")
+        return None
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
+        return None
+
+def read_json(archivo):
+    import json
+    try:
+        with open(archivo, 'r', encoding='utf8') as pacientes:
+            paciente_list = json.load(pacientes)
+            dict_pacientes = {}
+            for i,value in enumerate(paciente_list):
+                dict_pacientes[i]=value
+            return dict_pacientes
+    except FileNotFoundError:
+        print(f"El archivo {archivo} no se encontró.")
+        return None
+    except json.JSONDecodeError:
+        print(f"El archivo {archivo} no es un archivo JSON válido.")
+        return None
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
+        return None
+    
+def read_txt(archivo):
+    try:
+        with open(archivo, 'r') as resultados:
+            resultado = resultados.read()
+        sineso = resultado.replace("^", ",")
+        rsepa = sineso.split("\n")
+        dict_resultados = {}
+        for i, value in enumerate(rsepa):
+            dict_resultados[i] = value.split(',')
+        return dict_resultados
+    except FileNotFoundError:
+        print(f"El archivo {archivo} no se encontró.")
+        return None
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
+        return None
+
+        
+
+            
+
+
+
+
+# contenido_medicos = read_csv(data_medicos)
+# if contenido_medicos is not None:
+#     print(contenido_medicos)
+
+# contenido_pacientes = read_json(data_pacientes)
+# if contenido_pacientes is not None:
+#     dict_pacientes = {}
+# for i,value in enumerate(contenido_pacientes):
+#     dict_pacientes[i] = value  #diccionario de los pacientes
+
+read_txt(data_resultados)
---
+++ main.py
@@ -0,0 +1,25 @@
+from funtions import *
+import os
+
+ruta_carpeta = 'Tercer_parcial\Taller_monitor\Datos'
+
+for nombre_archivo in os.listdir(ruta_carpeta):
+    ruta_archivo = os.path.join(ruta_carpeta, nombre_archivo)
+
+    if ruta_archivo.endswith('.csv'):
+        print(f'Procesando archivo CSV: {ruta_archivo}')
+        dict_medicos = read_csv(ruta_archivo)
+
+    elif ruta_archivo.endswith('.txt'):
+        print(f'Procesando archivo TXT: {ruta_archivo}')
+        dict_resultados = read_txt(ruta_archivo)
+
+
+    elif ruta_archivo.endswith('.json'):
+        print(f'Procesando archivo JSON: {ruta_archivo}')
+        dict_pacientes = read_json(ruta_archivo)
+
+
+# print(dict_resultados)
+# print(dict_pacientes)
+print(dict_medicos)
